f82fa1882beafec623dc36eca70ea6e0b2f30f0e
DerekY123 <shadowstorm5127756@gmail.com>
2023-11-06 21:39:59 +0800

    draft

--- NutritionManagement/src/main/Day.java
+++ NutritionManagement/src/main/Day.java
@@ -2,20 +2,29 @@ package main;
 
 import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Day {
 	public Day(String dateString) {
-		//WIP
-		try{
-			this.date=LocalDate.parse(dateString);
-			this.foodPortions=new ArrayList<>();
+		// WIP
+		try {
+			this.date = LocalDate.parse(dateString);
+			this.foodPortions = new ArrayList<>();
+		} catch (Exception e) {
+
 		}
-		catch(Exception e){
-			
-		}
-		
+
 	}
-	
+
 	private LocalDate date;
 	private ArrayList<FoodPortion> foodPortions;
+
+	public NutritionValue getTotalNutrition() {
+		Stream<NutritionValue> nutritions = foodPortions.stream().map(foodPortion -> foodPortion.getNutrition());
+		return new NutritionValue(
+				nutritions.map(item -> item.getCalories()).reduce(0.0, (prev, curr) -> prev + curr),
+				nutritions.map(item -> item.getProtein()).reduce(0.0, (prev, curr) -> prev + curr),
+				nutritions.map(item -> item.getCarbs()).reduce(0.0, (prev, curr) -> prev + curr),
+				nutritions.map(item -> item.getFat()).reduce(0.0, (prev, curr) -> prev + curr));
+	}
 }
--- NutritionManagement/src/main/FoodPortion.java
+++ NutritionManagement/src/main/FoodPortion.java
@@ -1,11 +1,20 @@
 package main;
 
 public class FoodPortion {
-    public FoodPortion(Food food, double quantity){
-        this.food=food;
-        this.quantity=quantity;
+    public FoodPortion(Food food, double quantity) {
+        this.food = food;
+        this.quantity = quantity;
     }
 
     private Food food;
     private double quantity;
+
+    public NutritionValue getNutrition() {
+        NutritionValue unitNutritionValue = this.food.getNutritionValue();
+        return new NutritionValue(
+                unitNutritionValue.getCalories() * quantity,
+                unitNutritionValue.getProtein() * quantity,
+                unitNutritionValue.getCarbs() * quantity,
+                unitNutritionValue.getFat() * quantity);
+    }
 }
