14ec71afc0d3f7b2f6f60083b231d737aabac8da
DerekY123 <shadowstorm5127756@gmail.com>
2023-11-22 21:32:25 +0800

    output

--- NutritionManagement/src/io/OutputFormatter.java
+++ NutritionManagement/src/io/OutputFormatter.java
@@ -6,10 +6,14 @@ public class OutputFormatter {
     public static void printTotalNutritionValue(NutritionValue nutritionValue) {
         System.out.println("Total Nutrition Value:");
         System.out.println("----------------------");
-        System.out.println("Calories: " + formatDouble(nutritionValue.getCalories()) + " kcal");
-        System.out.println("Protein: " + formatDouble(nutritionValue.getProtein()) + " g");
-        System.out.println("Carbs: " + formatDouble(nutritionValue.getCarbs()) + " g");
-        System.out.println("Fat: " + formatDouble(nutritionValue.getFat()) + " g");
+        System.out.println("Calories: " + formatDouble(nutritionValue.getCalories()) + " kcal, "
+                + formatPercentage(nutritionValue.getCalories()));
+        System.out.println("Protein: " + formatDouble(nutritionValue.getProtein()) + " g, "
+                + formatPercentage(nutritionValue.getProtein()));
+        System.out.println("Carbs: " + formatDouble(nutritionValue.getCarbs()) + " g, "
+                + formatPercentage(nutritionValue.getCarbs()));
+        System.out.println("Fat: " + formatDouble(nutritionValue.getFat()) + " g, "
+                + formatPercentage(nutritionValue.getFat()));
         System.out.println();
     }
 
@@ -25,5 +29,9 @@ public class OutputFormatter {
 
     private static String formatDouble(double value) {
         return String.format("%.2f", value);
+    }
+
+    private static String formatPercentage(double value) {
+        return String.format("%.2f%%", value * 100);
     }
 }
