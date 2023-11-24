1b45f62a79bc802527ec7ee1629d3f05f4088836
DerekY123 <shadowstorm5127756@gmail.com>
2023-11-24 10:18:37 +0800

    more commands and bug fix

--- NutritionManagement/src/main/Main.java
+++ NutritionManagement/src/main/Main.java
@@ -29,14 +29,22 @@ public class Main {
             String cmdLine = in.nextLine().trim();
             if (cmdLine.equals(""))
                 continue;
-            String[] cmdParts = cmdLine.split("\\|"); //commands should be like: addFoodPortion|fish|100
+            String[] cmdParts = cmdLine.split("\\|"); // commands should be like: addFoodPortion|fish|100
             String cmd = cmdParts[0];
             try {
                 switch (cmd) {
                     case "addDay":
+                    case "switchToDay":
+                    case "importFoodTypes":
                         if (cmdParts.length < 2) {
                             throw new InsufficientArgumentsException();
                         }
+                        break;
+                    case "addPortion":
+                        if (cmdParts.length < 3) {
+                            throw new InsufficientArgumentsException();
+                        }
+                        break;
                 }
 
                 if (cmd.equals("addDay")) {
@@ -44,12 +52,19 @@ public class Main {
                     user.addDay(day);
                     currentDay = day;
                 } else if (cmd.equals("switchToDay")) {
-                    Day day = user.findDay(cmd);
+                    Day day = user.findDay(cmdParts[1]);
                     currentDay = day;
-                } else if (cmd.equals("Import food types")) {
+                } else if (cmd.equals("importFoodTypes")) {
                     String foodFilePath = cmdParts[1];
                     ArrayList<Food> foodTypes = InputParser.parseInputFoodFile(foodFilePath);
                     foodTypes.stream().forEach(food -> user.addFoodType(food));
+                } else if (cmd.equals("addPortion")) {
+                    if (currentDay == null)
+                        throw new DayNotSelectedException();
+                    String foodName = cmdParts[1];
+                    double amount = Double.parseDouble(cmdParts[2]);
+                    Food food = user.getFoodType(foodName);
+                    currentDay.upsertFoodPortion(food, amount);
                 } else if (cmd.equals("quit")) {
                     in.close();
                     break;
